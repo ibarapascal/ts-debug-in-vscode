e14b1e97b868fe441304098ec0ea0437b6dadb1d
Keikai <pascaljing@live.com>
2019-08-01 14:07:59 +0900

    Add: I/O for JSON

---
+++ src/io-import-data-from-json.ts
@@ -0,0 +1,25 @@
+/** Settings
+package.json
+{
+  "devDependencies": {
+    "typescript": "^2.9.2"
+tsconfig.json
+{
+    "compilerOptions": {
+        "resolveJsonModule": true,
+        "esModuleInterop": true
+*/
+
+import data from './data_source/import-data-from-json-test.json';
+
+interface testDataObject {
+    id: number;
+    value: number;
+    name: string;
+}
+
+let importData: testDataObject[] = data.data;
+
+for (let item of importData) {
+    console.log(item);
+}
